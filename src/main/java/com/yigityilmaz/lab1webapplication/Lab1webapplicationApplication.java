3d77e834609dc0165db8e5ee219253da04009061
Yigit Yilmaz <tahayigityilmaz@gmail.com>
2018-10-02 11:04:38 +0300

    Empty Project.

---
+++ src/main/java/com/yigityilmaz/lab1webapplication/Lab1webapplicationApplication.java
@@ -0,0 +1,12 @@
+package com.yigityilmaz.lab1webapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1webapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1webapplicationApplication.class, args);
+    }
+}
---
+++ src/test/java/com/yigityilmaz/lab1webapplication/Lab1webapplicationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yigityilmaz.lab1webapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lab1webapplicationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
