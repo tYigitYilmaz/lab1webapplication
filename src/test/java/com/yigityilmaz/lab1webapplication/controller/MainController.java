b78ae9cdde3d47cd8f9f73ed3f71ccd628215912
Yigit Yilmaz <tahayigityilmaz@gmail.com>
2018-10-02 11:07:09 +0300

    Main controller was included.

---
+++ src/test/java/com/yigityilmaz/lab1webapplication/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.yigityilmaz.lab1webapplication.controller;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @RequestMapping("/name")
+    public String helloUser(@PathVariable String name) {
+        return "Hello " + name;
+    }
+}
